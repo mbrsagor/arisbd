cc21426466d043e5018d478820bedb905bd6e1f2
Sagor <mbrsagor@gmail.com>
2020-10-06 19:36:56 +0600

    Base controler implement

---
+++ static/views/base/baseController.js
@@ -0,0 +1,23 @@
+angular
+  .module("Base")
+  .controller("baseController", function(
+    $scope,
+    profileResource,
+    UserListResource,
+    ActiveprofileResource){
+    "use strict";
+
+    $scope.pageTitle = "Base";
+    
+    profileResource.query().$promise.then(function(response) {
+      $scope.users = response;
+    });
+    
+    UserListResource.query().$promise.then(function (response) {
+      $scope.auth_user = response;
+    });
+    
+    ActiveprofileResource.my_profile().$promise.then(function(response) {
+      $scope.profile = response;
+    });
+  });
