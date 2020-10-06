7faa7d3df5321a6a6cc99b6f605dd9b92b318e2e
Sagor <mbrsagor@gmail.com>
2020-10-06 17:04:44 +0600

    Implement theme

--- arisbd/urls.py
+++ arisbd/urls.py
@@ -22,9 +22,8 @@ from django.urls import path, include, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-path('', TemplateView.as_view(template_name='index.html')),
+    path('', TemplateView.as_view(template_name='index.html')),
 ]
-
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
