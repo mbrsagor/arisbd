cb938e74f1cbe632b34b8a4d5d6d233fa14c6559
Sagor <mbrsagor@gmail.com>
2020-10-06 16:58:42 +0600

    Missing URL added

--- arisbd/urls.py
+++ arisbd/urls.py
@@ -13,9 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import TemplateView
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+path('', TemplateView.as_view(template_name='index.html')),
 ]
+
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
