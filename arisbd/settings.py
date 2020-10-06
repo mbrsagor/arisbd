7946b1bca01cefcc23671264c8930072775e886d
Sagor <mbrsagor@gmail.com>
2020-10-06 18:10:57 +0600

    Rest auth API registration with token

--- arisbd/settings.py
+++ arisbd/settings.py
@@ -41,7 +41,14 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'rest_auth',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'rest_auth.registration',
 ]
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- arisbd/urls.py
+++ arisbd/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
@@ -23,6 +24,7 @@ from django.urls import path, include, re_path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('rest_auth.urls')),
+    path('api/rest-auth/registration', include('rest_auth.registration.urls')),
     path('', TemplateView.as_view(template_name='index.html')),
 
 ]
