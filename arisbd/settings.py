76997e00a6fedf7d3649ed4089b705e9380c847d
Sagor <mbrsagor@gmail.com>
2020-10-31 18:43:54 +0600

    Token authentication system and default pagiantion

--- arisbd/settings.py
+++ arisbd/settings.py
@@ -112,11 +112,10 @@ STATICFILES_DIRS = [
 
 # Authentication
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
-    'DEFAULT_AUTHENTICATION_CLASSES': (
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 8,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-    )
+        # 'rest_framework.authentication.SessionAuthentication',
+    ]
 }
